173a6ec833a9d2154b24b362e18061250b080ae8
Ali El-Khatib <elkhatib.aliwalid@gmail.com>
2025-08-28 20:43:07 -0400

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="bg-white border-b border-gray-900/10">
+        <div className="mx-auto w-full p-4 flex items-center justify-center sm:justify-start">
+            <h1 className="text-[#29AB70] text-2xl sm:text-3xl md:text-4xl font-bold font-mono tracking-tight">
+                Origin Checker
+            </h1>
+        </div>
+    </header>
+  );
+};
+export default Header;
---
+++ frontend/src/components/TextInput.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function TextInput() {
+    const [text, setText] = useState("");
+
+    const handleInput = (e) => {
+      setText(e.target.value);
+    }
+
+   return (
+        <div className="w-full max-w-2xl mx-auto mb-4 border-4 border-[#9BAEC4]/10 rounded-lg bg-[#9BAEC4] dark:bg-gray-700 dark:border-gray-600">
+            <form>
+                <div className="h-full px-4 py-2 bg-[#F5F3F6] rounded-b-lg dark:bg-gray-800">
+                    <label htmlFor="editor" className="sr-only">Publish post</label>
+                    <textarea id="editor" rows="8" className="block w-full px-0 text-sm text-gray-800 bg-[#F5F3F6] border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 focus:outline-none" placeholder="Paste text here" required onChange={handleInput}></textarea>
+                </div>
+                <div className="flex justify-end space-x-1 rtl:space-x-reverse sm:pe-4 px-6 py-2">
+                    <button className="bg-[#3A9D9D] text-white px-5 py-2 rounded-lg 
+             hover:bg-[#2C7A7A] cursor-pointer
+             disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:bg-gray-400
+             transition-colors duration-200 shadow-md" disabled={!text.trim()}>
+                        Analyze Text
+                    </button>
+                </div>
+            </form>
+        </div>
+
+    )
+}
+
+export default TextInput
